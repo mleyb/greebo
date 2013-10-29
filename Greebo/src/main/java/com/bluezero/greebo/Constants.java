9cde6f77b25a63fc20ee168c7fd79dba7f9d35f7
mley <mleybourne1@gmail.com>
2013-10-29 10:36:48 +0000

    Update Constants.java

--- Greebo/src/main/java/com/bluezero/greebo/Constants.java
+++ Greebo/src/main/java/com/bluezero/greebo/Constants.java
@@ -1,8 +1,5 @@
 package com.bluezero.greebo;
 
-/**
- * Created by LeybourneM on 23/10/13.
- */
 public class Constants {
     public static final String ServiceUri = "https://ringo.azure-mobile.net/";
     public static final String ApiKey = "PsJWAoDSLCaSHfPhlnnkqPMKFekViw10";
